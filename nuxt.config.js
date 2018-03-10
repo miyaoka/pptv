02d6b6b5f40f7491f9cdfcba9a311c853c6c1af1
Masaya Kazama <miyan@t-p.jp>
2018-03-10 13:51:57 +0900

    Add ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,9 +13,32 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'pptv' }
+      { hid: 'description', name: 'description', content: 'プープーテレビの全動画を検索します' },
+      {
+        property: 'og:title',
+        content: 'プープーテレビ検索',
+        hid: 'ogTitle'
+      },
+      { property: 'og:site_name', content: 'プープーテレビ検索' },
+      { property: 'og:url', content: 'https://pptv.netlify.com', hid: 'ogUrl' },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:locale', content: 'ja_JP' },
+      {
+        property: 'og:description',
+        content: `プープーテレビの全動画を検索します`,
+        hid: 'ogDesc'
+      },
+      {
+        property: 'og:image',
+        content: 'https://pptv.netlify.com/img/pptv-ogp.png',
+        hid: 'ogImage'
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }
+    ]
   },
   /*
   ** Customize the progress bar color
